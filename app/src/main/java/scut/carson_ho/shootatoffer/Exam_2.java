f32878b81558d7ce7aecf558c2b961165a9bc48a
Carson.Ho <carson.ho@foxmail.com>
2017-10-19 11:19:19 +0800

    add 3

--- app/src/main/java/scut/carson_ho/shootatoffer/Exam_2.java
+++ app/src/main/java/scut/carson_ho/shootatoffer/Exam_2.java
@@ -7,6 +7,7 @@ package scut.carson_ho.shootatoffer;
 public class Exam_2 {
 
     /**
+     * 找出数组中重复数字（可修改数组）
      * @param arr = 输入数组
      * @return
      */
@@ -23,7 +24,7 @@ public class Exam_2 {
 
 
         for (int i = 0; i < arr.length; i++) {
-            if (arr[i] < 0 || arr[i] > (arr.length - 1)) {
+            if (arr[i] < 0 || arr[i] > arr.length) {
                 System.out.println("输入不合法");
                 return false;
             }
@@ -56,18 +57,118 @@ public class Exam_2 {
         return true;
     }
 
+
+    /*******************************************/
+
+
+    /**
+     * 找出数组中重复数字（不可修改数组）
+     * 方式：创建辅助数组
+     * @param arr = 输入数组
+     * @return
+     */
+
+     private static int answer_AssArr(int[] arr){
+         // 1. 判断输入数据的合法性
+         // 即，判断数组下标是否越界 & 数字 = 1~n 范围
+         if(arr==null||arr.length-1<=0)
+             return -1;
+         for (int i = 0; i < arr.length; i++) {
+             if(arr[i]<1||arr[i]>arr.length)
+                 return -1;
+         }
+         // 2. 创建辅助数组
+         int[] cp = new int[arr.length];
+
+         for (int i = 0; i < arr.length; i++) {
+             if(cp[arr[i]]==0)
+             	// 将原数组中的arr[i]复制到新数组中下标为arr[i]的位置
+                 cp[arr[i]] = arr[i];
+             else {
+                 System.out.println("重复的数字：" + arr[i]);
+                 // return arr[i];
+                 // 采用return，则是找出1个重复数字，则马上结束函数
+             }
+         }
+         System.out.println("结束");
+         return -1;
+     }
+
+
+    /*******************************************/
+
+    /**
+     * 找出数组中重复数字（不修改数组）
+     * 方式：二分查找
+     * @param arr = 输入数组
+     * @return
+     */
+    private static int answer_NoChangArr(int[] arr){
+        // 1. 判断输入数据 是否合法
+        // 即，判断数组下标是否越界 & 数字 = 1~n 范围
+        if(arr==null||arr.length==0)
+            return -1;
+
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i]<1||arr[i]>arr.length)
+                return -1;
+        }
+
+        int start = 1;
+        int end = arr.length-1;
+        while(start<=end){
+            // 2. 将范围数字分成2部分
+            int mid = (end+start)/2;
+            // 3. 统计区间内的数字出现的次数
+            int count = countRange(arr,arr.length-1,start,mid);
+            // 直到分到该部分只有1个数字 & 统计次数>1次，则找到重复数字，输出
+            if(end == start){
+                if(count>1) {
+                    System.out.println("重复的数字：" + start);
+                    return start;
+                }else
+                    break;
+            }
+            if(count>(mid-start+1))
+                end = mid;
+            else
+                start = mid+1;
+        }
+        System.out.println("结束");
+        return -1;
+    }
+
+    /**
+     * 统计区间范围内的数字出现的次数
+     */
+    private static int countRange(int[] arr, int n, int start, int end) {
+        if(arr==null||n<=0)
+            return 0;
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i]>=start&&arr[i]<=end)
+                count++;
+        }
+        return count;
+    }
+
+    /*******************************************/
+
     /**
      * 执行解题算法
      */
     public static void main(String[] args) {
 
         // 定义待解数列
-        int[] src = new int[]{2,3,1,0,2,5,3};
+//        int[] src = new int[]{2,3,1,0,2,5,3};
+        int[] src = new int[]{2,3,5,4,3,2,6,7};
 
         // 输出结果
-        answer(src);
+//        answer(src);
+        // answer_AssArr(src);
+        answer_NoChangArr(src);
+
 
     }
-
 
 }
