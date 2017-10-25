60611803c8e6d54e8378f1864dcc70054862a29d
Carson.Ho <carson.ho@foxmail.com>
2017-10-25 10:05:03 +0800

    add 10 finish

--- app/src/main/java/scut/carson_ho/shootatoffer/Exam_10.java
+++ app/src/main/java/scut/carson_ho/shootatoffer/Exam_10.java
@@ -38,6 +38,38 @@ public class Exam_10 {
         }
         return sum;
     }
+    
+    /**
+     * 青蛙跳台阶（基础）
+     */
+    public int jumpFloor(int number) {
+        if(number<=2)
+            return number;
+        int jumpone=2; // 离所求的number的距离为1步的情况，有多少种跳法
+        int jumptwo=1; // 离所求的number的距离为2步的情况，有多少种跳法
+        int sum=0;
+        for(int i=3;i<=number;i++){
+            sum=jumptwo+jumpone;
+            jumptwo=jumpone;
+            jumpone=sum;
+        }
+        return sum;
+    }
+
+    /**
+     * 青蛙跳台阶（变式）
+     */
+    public int jumpFloor2(int num) {
+        if(num<=2)
+            return num;
+        int jumpone=2; // 前面一级台阶的总跳法数
+        int sum=0;
+        for(int i=3;i<=num;i++){
+            sum = 2*jumpone;
+            jumpone = sum;
+        }
+        return sum;
+    }
 
 
 }
