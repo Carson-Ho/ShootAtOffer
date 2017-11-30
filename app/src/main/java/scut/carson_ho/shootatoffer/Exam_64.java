8bf10cf062e455164fcdb4ad73d3fb102fef46f9
Carson.Ho <carson.ho@foxmail.com>
2017-11-30 11:18:35 +0800

    add 64

---
+++ app/src/main/java/scut/carson_ho/shootatoffer/Exam_64.java
@@ -0,0 +1,35 @@
+package scut.carson_ho.shootatoffer;
+
+/**
+ * Created by Carson_Ho on 17/11/30.
+ */
+
+public class Exam_64 {
+
+    /**
+     * 测试用例
+     */
+    public static void main(String[] args){
+        // 功能测试：1+2+....+5、1+2+....+10
+        System.out.println(Sum_Solution(5));
+        System.out.println(Sum_Solution(10));
+
+        // 特殊输入测试：0，1
+        System.out.println(Sum_Solution(0));
+        System.out.println(Sum_Solution(1));
+    }
+
+
+    /**
+     * 解题算法
+     */
+    public static int Sum_Solution(int n)
+    {
+        int sum = n;
+        // 递归的结束条件：n=0
+        // 递归执行语句：累加
+        boolean s = ((n > 0) && ((sum += Sum_Solution(n-1))>0));
+        return sum;
+    }
+
+}
