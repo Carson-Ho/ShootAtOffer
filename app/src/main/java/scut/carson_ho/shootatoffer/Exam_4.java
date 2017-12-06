e2c10ec348c3a428a0aadbb566cbd87f6174d168
Carson.Ho <carson.ho@foxmail.com>
2017-12-06 16:04:37 +0800

    add 4

--- app/src/main/java/scut/carson_ho/shootatoffer/Exam_4.java
+++ app/src/main/java/scut/carson_ho/shootatoffer/Exam_4.java
@@ -8,38 +8,42 @@ public class Exam_4 {
 
     /**
      * 在二维数组中查找
-     * @param arr = 二维数组
+     * @param array = 二维数组
      * @param target = 需查找的整数
      * @return 返回false代表该数组无该整数；返回true代表该数组有该整数
      */
 
-    private static boolean answer(int[][] arr,int target){
+    private static boolean answer(int[][] array,int target){
         // 1. 判断输入数据 是否合法
-        if(arr==null) {
-            System.out.println("输入数据不合法");
+        if(array==null) {
+            System.out.print("输入数据不合法");
             return false;
         }
 
-        // 2. 设置查找范围初始右上角整数的二维坐标
-        int row = 0;
-        int col = arr[0].length-1;
+        // 2. 设置查找范围初始右上角整数的二维坐标 = 最右上角元素
+        int row = 0; // 第1行
+        int col = array[0].length-1; // 最后列
 
-        int numRow = arr.length; // 二维数组的行数
+        int numRow = array.length; // 二维数组的行数
 
-        // 3. 比较查找范围右上角整数 与 需查找的证书
-        while(row<numRow && col>=0){
+        // 3. 比较查找范围右上角整数 与 需查找的整数
+        while(row < numRow && col>=0){
 
-            if(arr[row][col]>target)
-                col--;  // 去掉该列
-            else if (arr[row][col]<target) {
-                row++;  // 去掉该行
+            // 若 需查找的整数 < 右上角整数 ，则在当前查找范围中删除当前列
+            if(target < array[row][col])
+                col--;
+
+                // 若 需查找的整数 > 右上角整数 ，则在当前查找范围中删除当前行
+            else if (target > array[row][col]) {
+                row++;
+
             }else {
-                System.out.println("数组中存在整数" + target);
-                // 表示二维数组存在该整数
+                // 若 需查找的整数 = 右上角整数表示二维数组存在该整数
+                System.out.print("数组中存在整数" + target);
                 return true;
             }
         }
-        System.out.println("数组中不存在整数" + target);
+        System.out.print("数组中不存在整数" + target);
         return false;
     }
 
@@ -48,12 +52,12 @@ public class Exam_4 {
      */
     public static void main(String[] args) {
 
-        // 定义待解数列
+        // 功能测试
         int[][] src = new int[][]{{1,2,8,9},{2,4,9,12},{4,7,10,13},{6,8,11,15}};
+        System.out.println(answer(src,7));
 
-        // 执行整数
-        answer(src,7);
-
+        // 特殊输入测试
+        System.out.println(answer(null,7));
 
     }
 
