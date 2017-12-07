ce47f336f17428ea7f7ab3dd3f88b613f34cd607
Carson.Ho <carson.ho@foxmail.com>
2017-12-07 14:32:57 +0800

    add 8

--- app/src/main/java/scut/carson_ho/shootatoffer/Exam_8.java
+++ app/src/main/java/scut/carson_ho/shootatoffer/Exam_8.java
@@ -6,6 +6,74 @@ package scut.carson_ho.shootatoffer;
 
 public class Exam_8 {
 
+    /**
+     * 解题算法：获取下1个结点算法
+     */
+
+    private static TreeLinkNode getNext(TreeLinkNode pNode) {
+
+        // 1. 判断输入的结点的合法性
+        if (pNode == null)
+            return null;
+
+        TreeLinkNode cur = null;
+
+
+        // 2. 判断输入结点是否有右子树
+        if (pNode.right != null) {
+
+            cur = pNode.right;
+
+            // 若具备右子树，找出其右子树节点的 最 左边的子树节点
+            while (cur.left != null) {
+                // 下1个结点 = 其右子树 的最左子结点
+                cur = cur.left;
+            }
+            return cur;
+        }
+
+        // 3. 若该节点没有右子树，则判断其是否 是其父节点的左子树
+        if (pNode.next == null) {
+            return null;
+        }
+
+        // 若是其父节点的左子树，下1个结点 = 其父结点
+        if (pNode == pNode.next.left) {
+
+            return pNode.next;
+        }
+
+        // 4. 若该节点无右子树 &  不是其父节点的左子树
+        // 向上寻找其父节点，直到找到父节点是其父节点的左孩子为此，此时，下一个节点为最后那个父节点
+        while (pNode.next != null) {
+            if (pNode == pNode.next.left) {
+                return pNode.next;
+            }
+            // 继续寻找
+            pNode = pNode.next;
+        }
+        return null;
+
+    }
+
+    /**
+     * 辅助类：建立二叉树节点类
+     */
+
+    static class TreeLinkNode {
+        String val;
+        TreeLinkNode left;
+        TreeLinkNode right;
+        TreeLinkNode next;// 此处的next = 指向父节点
+        public TreeLinkNode(String val) {
+            this.val = val;
+        }
+
+    }
+
+    /**
+     * 测试用例
+     */
     public static void main(String[] args) {
         // 定义树结构
 
@@ -17,110 +85,49 @@ public class Exam_8 {
 //              / \
 //             h   i
 
-        TreeNode root = new TreeNode("a");
-        TreeNode r2 = new TreeNode("b");
-        TreeNode r3 = new TreeNode("c");
-        TreeNode r4 = new TreeNode("d");
-        TreeNode r5 = new TreeNode("e");
-        TreeNode r6 = new TreeNode("f");
-        TreeNode r7 = new TreeNode("g");
-        TreeNode r8 = new TreeNode("h");
-        TreeNode r9 = new TreeNode("i");
+        TreeLinkNode root = new TreeLinkNode("a");
+        TreeLinkNode r2 = new TreeLinkNode("b");
+        TreeLinkNode r3 = new TreeLinkNode("c");
+        TreeLinkNode r4 = new TreeLinkNode("d");
+        TreeLinkNode r5 = new TreeLinkNode("e");
+        TreeLinkNode r6 = new TreeLinkNode("f");
+        TreeLinkNode r7 = new TreeLinkNode("g");
+        TreeLinkNode r8 = new TreeLinkNode("h");
+        TreeLinkNode r9 = new TreeLinkNode("i");
 
         root.left = r2;
         root.right = r3;
 
-        r2.parent = root;
+        r2.next = root;
         r2.left = r4;
         r2.right = r5;
 
-        r3.parent = root;
+        r3.next = root;
         r3.right = r6;
         r3.left = r7;
 
-        r4.parent = r2;
-        r5.parent = r2;
+        r4.next = r2;
+        r5.next = r2;
         r5.right = r9;
         r5.left = r8;
 
-        r7.parent = r3;
-        r6.parent = r3;
+        r7.next = r3;
+        r6.next = r3;
 
-        r8.parent = r5;
-        r9.parent = r5;
+        r8.next = r5;
+        r9.next = r5;
 
         // 测试该结点有右子树的情况（b）
-        System.out.println(getNextTreeNode(r2).val);
+        System.out.println(getNext(r2).val);
 
         // 测试该结点无 右子树 & 是其父结点的左子树（d）
-        System.out.println(getNextTreeNode(r4).val);
+        System.out.println(getNext(r4).val);
 
         // 测试该结点无 右子树 & 是其父结点的右子树（i）
-        System.out.println(getNextTreeNode(r9).val);
+        System.out.println(getNext(r9).val);
 
     }
-
-
-
-    /**
-     * 获取下1个结点算法
-     */
-
-    private static TreeNode getNextTreeNode(TreeNode targetNode) {
-        // 1. 判断输入的结点的合法性
-        if (targetNode == null)
-            return null;
-        TreeNode cur = null;
-        // 2. 判断输入结点是否有右孩子
-        //
-        if (targetNode.right != null) {
-            cur = targetNode.right;
-
-            // 若具备右孩子，找出其右孩子节点的 最 左边孩子节点
-            while (cur.left != null) {
-                // 设置下1个结点 = 其右子树 的最左子结点
-                cur = cur.left;
-            }
-            return cur;
-        }
-
-        // 3. 若该节点没有右孩子节点，则判断其是否是其父节点的左孩子
-        if (targetNode.parent == null) {
-            return null;
-        }
-        if (targetNode == targetNode.parent.left) {
-            // 若是其父节点的左孩子，就设置下1个结点 = 其父结点
-            return targetNode.parent;
-        }
-
-        // 3. 此时，该节点既没有右孩子节点，也不是其父节点的左孩子
-        // 向上寻找其父节点，直到找到父节点是其父节点的左孩子为此，此时，下一个节点为最后那个父节点
-        while (targetNode.parent != null) {
-            if (targetNode == targetNode.parent.left) {
-                return targetNode.parent;
-            }
-            // 继续寻找
-            targetNode = targetNode.parent;
-        }
-        return null;
-
-    }
-
 
 }
 
-/**
- * 建立二叉树节点类
- */
 
-class TreeNode {
-    String val;
-    TreeNode left;
-    TreeNode right;
-    TreeNode parent;
-
-    public TreeNode(String val) {
-        this.val = val;
-    }
-
-}
