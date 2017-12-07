f64b07373035c352fac05c9baba5a909f56fb9b4
Carson.Ho <carson.ho@foxmail.com>
2017-12-07 22:04:20 +0800

    add 13

--- app/src/main/java/scut/carson_ho/shootatoffer/Exam_11.java
+++ app/src/main/java/scut/carson_ho/shootatoffer/Exam_11.java
@@ -6,53 +6,46 @@ package scut.carson_ho.shootatoffer;
 
 public class Exam_11 {
 
-    public static void main(String[] args) {
-
-        // 输入旋转后的数组
-        int[] src = new int[]{3,4,5,1,2};
-        // 输出结果
-        System.out.println(getMin(src));
-    }
-
-
     /**
-     * 利用二分查找的思想，时间复杂度为O(logn)。
+     * 解题算法
+     * 原理：二分查找，时间复杂度为O(logn)。
      * 注意对特殊情况的处理：特殊情况1 （旋转数组= 有序）、特殊情况2（两指针指向元素 与 中间元素 相等）
      * @param arr
      * @return
      */
-    public static int getMin(int[] arr){
+    public static int minNumberInRotateArray(int[] arr){
 
         // 1. 设置2个指针的初始值
         int index1 = 0; // 第1个指针设置为：旋转数组的第1个元素 = 前子数组的第1个元素
         int index2 = arr.length-1; // 第2个指针设置为：旋转数组的最后1个元素 = 后子数组的最后1个元素
 
         // 2. 初始化中间元素值
-        // 为了兼顾 特殊情况1（旋转数组= 有序），故将将中间元素初始化为第1个元素
+        // 为了兼顾 特殊情况1（旋转数组= 有序），故将中间元素初始化为第1个元素
         int mid = 0;
-
-
-        while(arr[index1]>=arr[index2]){
+        // 一旦发现数组第1个数字 < 最后1个数字，即说明旋转数组 = 有序
+        // 则直接跳出循环，直接输出第1个数字
+        while( arr[index1] >= arr[index2] ){
             // 若2个指针距离 = 1，即相邻时，则代表
               // a. 第1个指针指向的元素 = 前子数组的最后1个元素
               // b. 第2个指针指向的元素 = 后子数组的第1个元素,即，数组中最小的元素，此时直接输出第2个指针元素，并跳出循环
-            if(index2-index1==1){
+            if( index2 - index1 == 1 ){
                 mid = index2;
                 break;
             }
 
-            // 若2个指针距离 ≠ 1，即开始算法
+            // 若2个指针距离 ≠ 1，即开始解题算法
             // 3. 找出中间元素
-            mid = (index1+index2)/2;
+            mid = index1 + (index2-index1) / 2;
+
             // 为了兼顾特殊情况2（两指针指向元素 与 中间元素 相等），则只能顺序查找
-            if(arr[index1]==arr[index2]&&arr[mid]==arr[index1]){
+            if( arr[index1]==arr[index2] && arr[mid]==arr[index1] ){
                 return inInOrder(arr,index1,index2);
             }
 
             // 4. 开始比较中间元素 与 指针元素 的大小
-            if(arr[mid]>=arr[index1]){
+            if( arr[mid]>=arr[index1] ){
                 index1 = mid;
-            }else if(arr[mid]<=arr[index2]){
+            }else if( arr[mid]<=arr[index2] ){
                 index2 = mid;
             }
         }
@@ -62,7 +55,7 @@ public class Exam_11 {
     }
 
     /**
-     * 顺序查找算法
+     * 辅助算法：顺序查找算法
      */
     public static int inInOrder(int[] arr, int index1, int index2) {
         int min = arr[index1];
@@ -71,5 +64,16 @@ public class Exam_11 {
                 min = arr[i];
         }
         return min;
+    }
+
+    /**
+     * 测试用例
+     */
+    public static void main(String[] args) {
+
+        // 输入旋转后的数组
+        int[] src = new int[]{3,4,5,1,2};
+        // 输出结果
+        System.out.println(minNumberInRotateArray(src));
     }
 }
--- app/src/main/java/scut/carson_ho/shootatoffer/Exam_12.java
+++ app/src/main/java/scut/carson_ho/shootatoffer/Exam_12.java
@@ -7,48 +7,30 @@ package scut.carson_ho.shootatoffer;
 public class Exam_12 {
 
     /**
-     * 执行回溯算法
-     */
-    public static void main(String[] args){
-        // 定义矩阵 = 二维数组
-        char[][] data = {
-                {'a','b','t','g'},
-                {'c','f','c','s'},
-                {'j','d','e','h'}};
-
-        // 测试输出结果
-        System.out.println(hasPath(data,"bfce")); //true
-        System.out.println(hasPath(data,"abfb")); //false,访问过的位置不能再访问
-    }
-
-    /**
-     * 回溯算法实现
-     * 实现原理 = 递归
-     * @param data = 矩阵
+     * 解题算法
+     * 实现原理 = 回溯法，采用递归实现
+     * @param matrix = 矩阵
+     * param row = 矩阵行数
+     * param cols = 矩阵列数
      * @param str = 需寻找的字符串路径
      * @return boolean = 矩阵中是否存在要寻找的字符串路径
      */
-    public static boolean hasPath(char[][] data,String str){
-        // 1. 判断数据的合法性
-        if(data==null || data.length==0 || str==null || str.length()==0)
+
+    public static boolean hasPath(char[] matrix, int rows, int cols, char[] str) {
+
+        // 判断输入数据的合法性
+        if(matrix == null || str == null || matrix.length == 0 || str.length == 0)
             return false;
 
-        int rowLen = data.length; // 矩阵行数
-        int colLen = data[0].length;// 矩阵列数
+        // 定义1个矩阵，用于标识路径是否已进入每个格子
+        // 0表示未访问、1表示访问过
+        int flag[] = new int[matrix.length];
 
-        // 2. 定义1个布尔值矩阵，用于标识路径是否已进入每个格子
-        boolean[][] visitFlag = new boolean[rowLen][colLen];
-        // 初始化布尔值矩阵的初始值，即都未访问过
-        for(int row=0;row<rowLen;row++){
-            for(int col=0;col<colLen;col++){
-                visitFlag[row][col] = false;
-            }
-        }
-
-        // 3. 通过循环寻找矩阵中的值 与 字符串路径中相等的值
-        for(int row=0;row<rowLen;row++){
-            for(int col=0;col<colLen;col++){
-                if(hasPathCore(data,row,col,visitFlag,str,0))
+        // 通过遍历比较矩阵中的值 & 字符串路径中的值是否相等
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                // 通过递归判断是否相等
+                if (helper(matrix, rows, cols, i, j, str, 0, flag))
                     return true;
             }
         }
@@ -56,54 +38,59 @@ public class Exam_12 {
     }
 
     /**
-     * 判断 矩阵中是否存在于需寻找的字符串路径
+     * 辅助算法：判断 矩阵中是否存在于需寻找的字符串路径
      * 实现原理 = 递归
-     * @param data = 矩阵
-     * param rowIndex = 当前行数
-     * param colIndexIndex = 当前列数
-     * param visitFlag = 路径标识符
+     * @param matrix = 矩阵
+     * param row = 矩阵行数
+     * param cols = 矩阵列数
+     * param i = 当前行
+     * param j = 当前列
      * param str = 需寻找的字符串路径
-     * param strIndex = 字符串路径下标
+     * param k = 字符串路径下标
+     * param flag = 路径标识矩阵
      * @return boolean = 矩阵中是否存在要寻找的字符串路径
      */
-    public static boolean hasPathCore(char[][] data,int rowIndex,int colIndex,
-                                      boolean[][] visitFlag,String str,int strIndex){
-        // 结束条件
-        if(strIndex>=str.length()) return true;
+    private static boolean helper(char[] matrix, int rows, int cols, int i, int j, char[] str, int k, int[] flag) {
 
-        if(rowIndex<0 || colIndex<0 || rowIndex>=data.length || colIndex>=data[0].length)
+        // 当前矩阵比较元素
+        int index = i * cols + j;
+
+        // 判断矩阵值是否与路径中的值相等
+        if (i < 0 || i >= rows || j < 0 || j >= cols || matrix[index] != str[k] || flag[index] == 1)
             return false;
 
-        // 递归
-        // 若该矩阵值未被访问，且匹配字符串，就：
-        // a. 标记当前位置为已访问
-        // b. 分上下左右四个方向位置通过递归继续寻找字符串路径中下1个值
-        if(!visitFlag[rowIndex][colIndex]&&data[rowIndex][colIndex]==str.charAt(strIndex)){
+        // 结束条件：即比较完毕
+        if(k == str.length - 1)
+            return true;
 
-            // a. 标记当前位置为已访问
-            visitFlag[rowIndex][colIndex] = true;
-
-            // b. 分上下左右四个方向位置通过递归继续寻找字符串路径中下1个值
-            boolean result =
-                    hasPathCore(data,rowIndex+1,colIndex,visitFlag,str,strIndex+1) ||
-                            hasPathCore(data,rowIndex-1,colIndex,visitFlag,str,strIndex+1) ||
-                            hasPathCore(data,rowIndex,colIndex+1,visitFlag,str,strIndex+1) ||
-                            hasPathCore(data,rowIndex,colIndex-1,visitFlag,str,strIndex+1);
-            // 若4个相邻格子匹配到字符串路径的下1个值，则视为当前递归的路线求解成功
-            // 无需修改标记
-            if(result)
-                return true;
-
-            else{
-                // 否则，即若4个相邻格子都无法匹配 到字符串路径的下1个值 成功
-                // 视当前递归的路线求解失败，需把这条线路上的标记清除掉
-                // 使得其他起点的路径依旧可访问本路径上的节点，方便重新定位
-                visitFlag[rowIndex][colIndex] = false;
-                return false;
-            }
+        flag[index] = 1;
+        // 分上下左右四个方向位置通过递归继续判断矩阵中的值是否与字符串路径中下1个值相等
+        if (helper(matrix, rows, cols, i - 1, j, str, k + 1, flag)
+                || helper(matrix, rows, cols, i + 1, j, str, k + 1, flag)
+                || helper(matrix, rows, cols, i, j - 1, str, k + 1, flag)
+                || helper(matrix, rows, cols, i, j + 1, str, k + 1, flag)) {
+            return true;
         }
-        else
-            return false;
+
+        flag[index] = 0;
+        return false;
+    }
+
+    /**
+     * 测试用例
+     */
+    public static void main(String[] args){
+        char[] matrix = { 'a', 'b', 't', 'g',
+                'c', 'f', 'c', 's',
+                'j', 'd', 'e', 'h'};
+        int rows = 3;
+        int cols = 4;
+
+        // 测试输出结果
+        System.out.println(hasPath(matrix, rows, cols, new char[] {'b','f','c','e'})); //true
+        System.out.println(hasPath(matrix, rows, cols, new char[] {'a','b','f','b'})); //false,访问过的位置不能再访问
+
+
     }
 
 }
