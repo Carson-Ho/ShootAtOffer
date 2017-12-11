819740c3ffbb719845b9fb2d0e6ba91924c6dff0
Carson.Ho <carson.ho@foxmail.com>
2017-12-11 14:25:17 +0800

    add 23

--- app/src/main/java/scut/carson_ho/shootatoffer/Exam_23.java
+++ app/src/main/java/scut/carson_ho/shootatoffer/Exam_23.java
@@ -7,65 +7,15 @@ package scut.carson_ho.shootatoffer;
 public class Exam_23 {
 
     /**
-     * 测试用例
-     */
-    public static void main(String[] args){
-        // 创建链表：
-        // 1->2->3->4->5->6
-        //       ^        |
-        //       |        |
-        //       +--------+
-        ListNode<Integer> head = new ListNode<Integer>(1);
-        ListNode<Integer> node2 = new ListNode<Integer>(2);
-        ListNode<Integer> node3 = new ListNode<Integer>(3);
-        ListNode<Integer> node4 = new ListNode<Integer>(4);
-        ListNode<Integer> node5 = new ListNode<Integer>(5);
-        ListNode<Integer> node6 = new ListNode<Integer>(6);
-        head.next = node2;
-        node2.next = node3;
-        node3.next = node4;
-        node4.next = node5;
-        node5.next = node6;
-        node6.next = node3;
-
-        // 功能测试：倒数第1个节点（尾结点）、倒数第6个节点（头节点）、中间节点
-        System.out.println(meetingNode(head).val);
-
-        // 异常测试：链表中没环
-        // 创建链表：
-        // 1->2->3->4->5->6
-//        ListNode<Integer> head = new ListNode<Integer>(1);
-//        ListNode<Integer> node2 = new ListNode<Integer>(2);
-//        ListNode<Integer> node3 = new ListNode<Integer>(3);
-//        ListNode<Integer> node4 = new ListNode<Integer>(4);
-//        ListNode<Integer> node5 = new ListNode<Integer>(5);
-//        ListNode<Integer> node6 = new ListNode<Integer>(6);
-//        head.next = node2;
-//        node2.next = node3;
-//        node3.next = node4;
-//        node4.next = node5;
-//        node5.next = node6;
-//
-//        System.out.println(meetingNode(head).val);
-
-
-        // 异常测试：头节点为空
-//        System.out.println(meetingNode(null).val);
-
-
-    }
-
-
-    /**
      * 设置结点结构
      */
-    public static class ListNode<T> {
-        T val;
-        ListNode<T> next;
 
-        public ListNode(T data) {
-            this.val = data;
-            this.next = null;
+    public static class ListNode {
+        int val;
+        ListNode next = null;
+
+        ListNode(int val) {
+            this.val = val;
         }
     }
 
@@ -74,10 +24,10 @@ public class Exam_23 {
      * 解题算法
      * 分为3个步骤：1.判断链表中是否有环；2.获得环中节点；3.找到环的入口节点
      */
-    public static ListNode<Integer> meetingNode(ListNode<Integer> head) {
+    public static ListNode meetingNode(ListNode pHead) {
 
         // 判断输入头节点的合法性
-        if(head == null) {
+        if(pHead == null) {
             System.out.println("头节点为空");
             return null;
         }
@@ -87,8 +37,8 @@ public class Exam_23 {
          */
 
         // 定义2个指针，同时从链表头节点出发
-        ListNode<Integer> fast = head;
-        ListNode<Integer> slow = head;
+        ListNode fast = pHead;
+        ListNode slow = pHead;
 
         // p1移动1步/次，p2移动2步/次
         while (fast != null && fast.next != null) {
@@ -102,8 +52,7 @@ public class Exam_23 {
 
         }
 
-        // 链表中没有环
-        // 若p2走到了链表的尾端依然未遇到p1，则代表无环，返回空
+        // 若p2走到了链表的尾端依然未遇到p1，则代表链表中无环，返回空
         if (fast == null || fast.next == null) {
             System.out.println("链表中无环");
             return null;
@@ -116,7 +65,7 @@ public class Exam_23 {
          * c. 当指针再次回到该相遇节点时，计数器即为环的数量
          */
 
-        int nodesLoop =1 ;
+        int nodesLoop =1 ; // 用于记录环节点的数量
         while(fast.next!= slow){
             fast = fast.next;
             ++ nodesLoop;
@@ -125,10 +74,14 @@ public class Exam_23 {
 
         /**
          * 步骤3：找到环的入口节点
+         * a. 定义2个指针p1、p2
+         * b. 同时从链表头节点出发：p1先移动n步，p2不动（n= 环的节点数量）
+         * c. 2指针同时往前移动，当2指针相遇时，相遇节点 = 环的入口节点
+         * 此时，p2到达环的入口节点、p1也已经围绕环走了1圈到达环的入口节点
          */
         // 将2个指针p1、p2重新回到链表头节点出发
-        fast = head;
-        slow = head;
+        fast = pHead;
+        slow = pHead;
 
         // p1先移动n步（n= 环的节点数量，此处n=4）,p2不动
         for (int i = 0;i < nodesLoop;++i){
@@ -142,5 +95,55 @@ public class Exam_23 {
             slow = slow.next;
         }
         return fast;
+    }
+
+
+    /**
+     * 测试用例
+     */
+    public static void main(String[] args){
+        // 创建链表：
+        // 1->2->3->4->5->6
+        //       ^        |
+        //       |        |
+        //       +--------+
+        ListNode pHead = new ListNode(1);
+        ListNode node2 = new ListNode(2);
+        ListNode node3 = new ListNode(3);
+        ListNode node4 = new ListNode(4);
+        ListNode node5 = new ListNode(5);
+        ListNode node6 = new ListNode(6);
+        pHead.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        node4.next = node5;
+        node5.next = node6;
+        node6.next = node3;
+
+        // 功能测试：倒数第1个节点（尾结点）、倒数第6个节点（头节点）、中间节点
+        System.out.println(meetingNode(pHead).val);
+
+        // 异常测试：链表中没环
+        // 创建链表：
+        // 1->2->3->4->5->6
+//        ListNode pHead = new ListNode(1);
+//        ListNode node2 = new ListNode(2);
+//        ListNode node3 = new ListNode(3);
+//        ListNode node4 = new ListNode(4);
+//        ListNode node5 = new ListNode(5);
+//        ListNode node6 = new ListNode(6);
+//        pHead.next = node2;
+//        node2.next = node3;
+//        node3.next = node4;
+//        node4.next = node5;
+//        node5.next = node6;
+//
+//        System.out.println(meetingNode(pHead).val);
+
+
+        // 异常测试：头节点为空
+//        System.out.println(meetingNode(null).val);
+
+
     }
 }
