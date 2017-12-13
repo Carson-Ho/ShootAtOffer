9cfc60ae0dae1116290443e2f166132733b4047e
Carson.Ho <carson.ho@foxmail.com>
2017-12-13 20:38:25 +0800

    add 42

--- app/src/main/java/scut/carson_ho/shootatoffer/Exam_42.java
+++ app/src/main/java/scut/carson_ho/shootatoffer/Exam_42.java
@@ -7,50 +7,49 @@ package scut.carson_ho.shootatoffer;
 public class Exam_42 {
 
     /**
-     * 测试用例
-     */
-    public static void main(String[] args){
-
-        // 功能测试：数组中无相同数字
-        int[] data = {1,-2,3,10,-4,7,2,-5};
-        System.out.println(findGreatestSumOfSumArrays(data));
-
-        // 特殊输入测试：数组为空
-        int[] data2 = null;
-        System.out.println(findGreatestSumOfSumArrays(data2));
-
-
-    }
-
-    /**
      * 解题算法：动态规划
      */
-    public static int findGreatestSumOfSumArrays(int[] data){
+    public static int findGreatestSumOfSumArrays(int[] array){
         // 1. 判断输入数据的合法性
-        if(data==null || data.length==0){
+        if(array==null || array.length==0){
             System.out.println("输入的数组为空");
             return 0;
         }
 
         // 2. 定义所需变量
-        //dp[i]：以data[i]为结尾元素的连续数组的最大和
-        //maxdp：当前最大dp值
-        int dp = data[0];
-        int maxdp = dp;
+        int dp = array[0]; // dp[i]：以array[i]为结尾元素的连续数组的最大和(初始化为第1个元素)
+        int maxdp = dp; // maxdp：当前dp的最大值
 
         // 3. 通过遍历数组进行动态规划
-        for(int i=1;i<data.length;i++){
+        for(int i=1;i < array.length;i++){
 
-            // 当以 第（ i - 1） 个数字结尾的子数组中所有数字的和 > 0时，最大值 = 等于 二者的和
-            if(dp>0)
-                dp += data[i];
+            // 3.1 当以 第（ i - 1） 个数字结尾的子数组中所有数字的和 > 0时，最大值 = 等于 二者的和
+            if( dp>0 )
+                dp += array[i];
             else
-                // 当以 第（ i - 1） 个数字结尾的子数组中所有数字的和 < 0时，最大值 = 其本身
-                dp = data[i];
+                // 3.2 当以 第（ i - 1） 个数字结尾的子数组中所有数字的和 < 0时，最大值 = 其本身
+                dp = array[i];
+            // 3.3 当前dp的最大值
             if(dp > maxdp)
                 maxdp = dp;
         }
         return maxdp;
+    }
+
+    /**
+     * 测试用例
+     */
+    public static void main(String[] args){
+
+        // 功能测试：数组中无相同数字
+        int[] array = {1,-2,3,10,-4,7,2,-5};
+        System.out.println(findGreatestSumOfSumArrays(array));
+
+        // 特殊输入测试：数组为空
+        int[] array2 = null;
+        System.out.println(findGreatestSumOfSumArrays(array2));
+
+
     }
 
 }
